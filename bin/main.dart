1ab3450bed308a88731f79ad8c89a4fc8a5124fe
Lemk <lemkin@ua.fm>
2020-01-28 16:48:41 +0200

    Step 002 for game.

--- bin/main.dart
+++ bin/main.dart
@@ -2,8 +2,6 @@ import 'package:guess/guess.dart' as guess;
 
 void main(List<String> arguments) {
   guess.welcome();
-  do {
-    guess.attempt();
-  } while (guess.isExit);
+  while (guess.attempt()){};
   guess.bye();
 }
